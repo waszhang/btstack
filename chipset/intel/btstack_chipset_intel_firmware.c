2ead266f19842aff7f234ad8ef8af4534073ac56
Matthias Ringwald <matthias@ringwald.ch>
2018-09-10 18:53:12 +0200

    chipset/intel: send firmware

--- chipset/intel/btstack_chipset_intel_firmware.c
+++ chipset/intel/btstack_chipset_intel_firmware.c
@@ -1,5 +1,8 @@
 #define __BTSTACK_FILE__ "btstack_chipset_intel_firmware.c"
 
+#include <fcntl.h>
+#include <unistd.h>
+
 #include "btstack_chipset_intel_firmware.h"
 #include "hci_cmd.h"
 #include "bluetooth.h"
@@ -13,6 +16,10 @@ const hci_transport_t * transport;
 static int state = 0;
 
 static uint8_t hci_outgoing[300];
+static uint8_t fw_buffer[300];
+
+static int fw_fd;
+static uint32_t fw_offset;
 
 typedef struct {
     uint8_t status;
@@ -69,11 +76,34 @@ static int transport_send_cmd(const hci_cmd_t *cmd, ...){
     return res;
 }
 
+static int transport_send_intel_secure(uint8_t fragment_type, const uint8_t * data, uint16_t len){
+    little_endian_store_16(hci_outgoing, 0, 0xfc09);
+    hci_outgoing[2] = 1 + len;
+    hci_outgoing[3] = fragment_type;
+    memcpy(&hci_outgoing[4], data, len);
+    uint16_t size = 3 +  1 + len;
+    hci_dump_packet(HCI_ACL_DATA_PACKET, 0, hci_outgoing, size);
+    return transport->send_packet(HCI_ACL_DATA_PACKET, hci_outgoing, size);
+}
+
+static void state_machine(uint8_t * packet);
+
+// read data from fw file and send it via intel_secure + update state
+static int intel_send_fragment(uint8_t fragment_type, uint16_t len){
+    int res = read(fw_fd, fw_buffer, len);
+    log_info("offset %6u, read %3u -> res %d", fw_offset, len, res);
+    fw_offset += res;
+    state++;
+    return transport_send_intel_secure(fragment_type, fw_buffer, len);
+}
+
 static void state_machine(uint8_t * packet){
     intel_version_t     * version;
     intel_boot_params_t * boot_params;
     uint16_t dev_revid;
     bd_addr_t addr;
+    char fw_name[30];
+    int res;
 
     switch (state){
         case 0:
@@ -105,7 +135,7 @@ static void state_machine(uint8_t * packet){
             transport_send_cmd(&hci_intel_read_secure_boot_params);
             state++;
             break;
-        default:
+        case 3:
             boot_params = (intel_boot_params_t *) hci_event_command_complete_get_return_parameters(packet);
             dev_revid = little_endian_read_16((uint8_t*)&boot_params->dev_revid, 0);
             reverse_bd_addr(boot_params->otp_bdaddr, addr);
@@ -119,13 +149,69 @@ static void state_machine(uint8_t * packet){
             // commmmand complete is required 
             if (boot_params->limited_cce != 0) break;
 
-            log_info("Ready to download firmware");
+            // firmware file
+            snprintf(fw_name, sizeof(fw_name), "ibt-11-%u.sfi", dev_revid);
+            log_info("Open firmware %s", fw_name);
+
+            // open firmware file
+            fw_fd = open(fw_name, O_RDONLY);
+            if (fw_fd < 0){
+                log_error("can't open file %s", fw_name);
+                return;
+            }
+
+            // send CCS segment - offset 0
+            intel_send_fragment(0x00, 128);
+            break;
+        case 4:
+            // send public key / part 1 - offset 128
+            intel_send_fragment(0x03, 128);
+            break;
+        case 5:
+            // send public key / part 2 - offset 384
+            intel_send_fragment(0x03, 128);
+            break;
+        case 6:
+            // skip 4 bytes
+            res = read(fw_fd, fw_buffer, 4);
+            log_info("read res %d", res);
+            fw_offset += res;
+
+            // send signature / part 1 - offset 388
+            intel_send_fragment(0x02, 128);
+            break;
+        case 7:
+            // send signature / part 2 - offset 516
+            intel_send_fragment(0x02, 128);
+            break;
+        case 8:
+            // send firmware chunks - offset 644
+            res = read(fw_fd, fw_buffer, 3);
+            log_info("read command, res %d", res);
+            if (res > 0 ){
+                int param_len = fw_buffer[2];
+                if (param_len){
+                    res = read(fw_fd, &fw_buffer[3], param_len);
+                }
+                transport_send_intel_secure(0x01, fw_buffer, param_len+3);
+                break;
+            }
+
+            // EOF
+            /* fall through */
+
+        default:
+
+            state++;
+            log_info("End of file");
+            close(fw_fd);
+            break;
     }    
 }
 
 static void transport_packet_handler (uint8_t packet_type, uint8_t *packet, uint16_t size){
     hci_dump_packet(packet_type, 1, packet, size);
-    if (packet_type != HCI_EVENT_PACKET) return;
+    // if (packet_type != HCI_EVENT_PACKET) return;
     switch (hci_event_packet_get_type(packet)){
         case HCI_EVENT_COMMAND_COMPLETE:
             state_machine(packet);
