274e44e4113d854b2c57e8d4d9715dbcb88cfdff
Matthias Ringwald <matthias@ringwald.ch>
2019-02-22 22:12:17 +0100

    update examples for split audio interface

--- example/headset_demo.c
+++ example/headset_demo.c
@@ -501,9 +501,9 @@ static int media_processing_init(avdtp_media_codec_configuration_sbc_t configura
     btstack_ring_buffer_init(&decoded_audio_ring_buffer, decoded_audio_storage, sizeof(decoded_audio_storage));
 
     // setup audio playback
-    const btstack_audio_t * audio = btstack_audio_get_instance();
-    if (audio){
-        audio->init(NUM_CHANNELS, configuration.sampling_frequency, &playback_handler, NULL);
+    const btstack_audio_sink_t * audio_sink = btstack_audio_sink_get_instance();
+    if (audio_sink){
+        audio_sink->init(NUM_CHANNELS, configuration.sampling_frequency, &playback_handler);
     }
 
     audio_stream_started = 0;
@@ -530,9 +530,9 @@ static void media_processing_close(void){
 #endif     
 
     // stop audio playback
-    const btstack_audio_t * audio = btstack_audio_get_instance();
-    if (audio){
-        audio->close();
+    const btstack_audio_sink_t * audio_sink = btstack_audio_sink_get_instance();
+    if (audio_sink){
+        audio_sink->close();
     }
 }
 
@@ -549,10 +549,10 @@ static void handle_l2cap_media_data_packet(uint8_t seid, uint8_t *packet, uint16
     avdtp_sbc_codec_header_t sbc_header;
     if (!read_sbc_header(packet, size, &pos, &sbc_header)) return;
 
-    const btstack_audio_t * audio = btstack_audio_get_instance();
+    const btstack_audio_sink_t * audio_sink = btstack_audio_sink_get_instance();
 
-    // process data right away if there's no audio implementation active, e.g. on posix systems to store as .wav
-    if (!audio){
+    // process data right away if there's no audio_sink implementation active, e.g. on posix systems to store as .wav
+    if (!audio_sink){
         btstack_sbc_decoder_process_data(&sbc_decoder_state, 0, packet+pos, size-pos);
         return;
     }
@@ -580,8 +580,8 @@ static void handle_l2cap_media_data_packet(uint8_t seid, uint8_t *packet, uint16
     if (!audio_stream_started && sbc_frames_in_buffer >= (OPTIMAL_FRAMES_MAX+OPTIMAL_FRAMES_MIN)/2){
         audio_stream_started = 1;
         // setup audio playback
-        if (audio){
-            audio->start_stream();
+        if (audio_sink){
+            audio_sink->start_stream();
         }
     }
 }
